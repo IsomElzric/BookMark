8d0be6f7760359bbc5436e402198c7e9fe841729
HollieGalb <83999037+HollieGalb@users.noreply.github.com>
2021-07-02 20:22:26 -0700

    HG

--- app/src/main/java/com/turner/bookmark/StarRating.java
+++ app/src/main/java/com/turner/bookmark/StarRating.java
@@ -1,5 +1,28 @@
 package com.turner.bookmark;
 
-public class StarRating {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StarRating extends AppCompatActivity {
+    RatingBar ratingBar;
+    String rateMessage;
+
+    protected void starRating(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ratingBar = findViewById(R.id.rating_bar);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener(){
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser){
+                rateMessage = String.valueOf(ratingBar.getRating());
+
+            }
+        });
+    }
 }
