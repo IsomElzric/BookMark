e1d1580d44fa7560e326374e26644c1f701ba6ac
Bonpaul <paul@paulw-eMachines-E527>
2021-07-06 21:45:11 +0000

    Here on I have made addition progress on the note activity. This is the activity where we will be giving the user the ability to add notes, update them, as well as delete them.

---
+++ app/src/main/java/com/turner/bookmark/Note.java
@@ -0,0 +1,38 @@
+package com.kabasele.activitynote;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Note_table")
+public class Note {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String title;
+
+    private String description;
+
+    private int priority;
+
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getPriority() {
+        return priority;
+    }
+
+}
